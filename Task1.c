165cd04e9a9db00a1f13cc473b58c52b3c89c750
MojoNojo06 <azaleah.j06@gmail.com>
2025-10-21 17:22:56 -0500

    added negative checking

--- Task1.c
+++ Task1.c
@@ -7,19 +7,24 @@ int main() {
         printf("Enter the NFL score (Enter 1 to stop): ");
         scanf("%f", &input);
 
-        // rat's nest loop
-        for (int twoPC = 0; twoPC <= (input/8); twoPC++) {
-            for (int onePC = 0; onePC <= (input/7); onePC++) {
-                for (int TD = 0; TD <= (input/6); TD++) {
-                    for (int FG = 0; FG <= (input/3); FG++) {
-                        for (int safety = 0; safety <= (input/2); safety++) {
-                            if ((TD * 6) + (FG * 3) + (safety * 2) + (twoPC * 8) + (onePC * 7) == input) {
-                                printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", twoPC, onePC, TD, FG, safety);
+        if(input < 0) {
+            printf("Input cannot be negative!\n");
+        } else {
+            // rat's nest loop
+            for (int twoPC = 0; twoPC <= (input/8); twoPC++) {
+                for (int onePC = 0; onePC <= (input/7); onePC++) {
+                    for (int TD = 0; TD <= (input/6); TD++) {
+                        for (int FG = 0; FG <= (input/3); FG++) {
+                            for (int safety = 0; safety <= (input/2); safety++) {
+                                if ((TD * 6) + (FG * 3) + (safety * 2) + (twoPC * 8) + (onePC * 7) == input) {
+                                    printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", twoPC, onePC, TD, FG, safety);
+                                }
                             }
                         }
                     }
                 }
             }
         }
+
     }
 }
